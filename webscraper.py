bd3968013cb6c284963cc52c959b67914af95538
andrayantelo <andrayantelo@gmail.com>
2020-04-18 16:18:46 -0700

    place each tweet into a JSON object

--- webscraper.py
+++ webscraper.py
@@ -5,8 +5,14 @@ url ='http://ethans_fake_twitter_site.surge.sh/'
 response = requests.get(url, timeout=5)
 soup = BeautifulSoup(response.content, "html.parser")
 
-tweet = soup.find_all('p', class_="content")
+tweets = soup.find_all('div', class_="tweetcontainer")
 
-for el in tweet:
-
-    print(el.text)
+for tweet in tweets:
+    tweetObject = {
+            "author": tweet.find('h2', class_="author").text.encode('utf-8'),
+            "date": tweet.find('h5', class_="dateTime").text.encode('utf-8'),
+            "tweet": tweet.find('p', class_="content").text.encode('utf-8'),
+            "likes": tweet.find('p', class_="likes").text.encode('utf-8'),
+            "shares": tweet.find('p', class_="shares").text.encode('utf-8')
+            }
+    print(tweetObject)
