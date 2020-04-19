0d83349f52f019dd2eede2ddbeb5a72468f21265
andrayantelo <andrayantelo@gmail.com>
2020-04-19 16:11:53 -0700

    split code into different functions

--- videoscraper.py
+++ videoscraper.py
@@ -1,19 +1,25 @@
+"""
+    turn
+
+    playlist = [ {'sources': [{},{}], 'image':'www.slkfjad.com', 'title': 'hello' }, ...]
+
+    into
+    [{'file': 'www.hello.com', 'title': 'art'}, {'file': 'www.world.com', 'title': 'art2'}]
+"""
 from bs4 import BeautifulSoup
 from yaml import safe_load
 import requests
 import json
 import re
 import sys
+import html
 
 pattern = re.compile("var jwPlaylists = \"(.*?)\";")
 
-with open('source.txt', 'r') as f:
-    urls = f.readlines()
+def get_title(soup):
+    return soup.title.text
 
-for url in urls:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-
+def get_playlist(soup):
     scripts = soup.find_all('script')
 
     script = [s for s in scripts if s.string and "jwPlaylists" in s.string]
@@ -21,21 +27,29 @@ for url in urls:
 
     m = re.search("jwPlaylists = (.*?]);", script.string)
     playlist = safe_load(m.group(1))
+    return playlist
 
-    #best = max(videos[0], key=lambda f: f['height']*f['width'])
-
-    def get_best(p):
-        best = max(p, key=lambda f: f['height']*f['width'])
-        return best
-
-    # playlist = [ {'sources': [{},{}], 'image':'www.slkfjad.com', 'title': 'hello' }, ...]
-
+def get_best_videos(playlist):
     videos = []
     for p in playlist:
         video = {}
         video['title'] = p['title']
-        video['file'] = get_best(p['sources'])['file']
+        best = max(p['sources'], key=lambda f: f['height']*f['width'])
+        video['file'] = best['file']
         videos.append(video)
+    return videos
 
-    print(videos)
-    # [{'file': 'www.hello.com', 'title': 'art'}, {'file': 'www.world.com', 'title': 'art2'}]
+def main():
+
+    with open('source.txt', 'r') as f:
+        urls = f.readlines()
+
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(html.unescape(response.text), "html.parser")
+        playlist = get_playlist(soup)
+        videos = get_best_videos(playlist)
+        print(videos)
+
+if __name__ == "__main__":
+    main()
