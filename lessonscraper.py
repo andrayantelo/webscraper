0dcbaccaef5f5b4b2e9f1d82df06cb742ef837ae
andrayantelo <andrayantelo@gmail.com>
2020-05-04 15:02:32 -0700

    scraper actually downloads video files, names them, and puts them into a directory

--- lessonscraper.py
+++ lessonscraper.py
@@ -14,19 +14,32 @@ import re
 import sys
 import html
 import os
+import logging
 
 pattern = re.compile("var jwPlaylists = \"(.*?)\";")
+logging.basicConfig(level=logging.DEBUG)
 
 def get_title(soup):
-    return soup.title.text
+    title = soup.title.text
+    return title.replace(' - New Masters Academy', '')
+
+def sanitize(filename):
+    filename = re.sub(r'\W+', '_', filename)
+    return filename
 
 def get_playlist(soup):
+    # returns list of videos
     scripts = soup.find_all('script')
 
     script = [s for s in scripts if s.string and "jwPlaylists" in s.string]
     script = script[0]
-
+    
     m = re.search("jwPlaylists = (.*?]);", script.string)
+    if not m:
+        title = get_title(soup)
+        logging.warning("jwPlaylists not found for %s", title)
+        return []
+    # safe_load reads Json and gives you back python
     playlist = safe_load(m.group(1))
     return playlist
 
@@ -38,6 +51,7 @@ def get_best_videos(playlist):
         best = max(p['sources'], key=lambda f: f['height']*f['width'])
         video['file'] = best['file']
         videos.append(video)
+    logging.info("Found %s videos", len(videos))
     return videos
 
 def download_videos(videos, lesson_title):
@@ -46,6 +60,19 @@ def download_videos(videos, lesson_title):
     video_directory = os.path.join(video_directory, lesson_title)
     os.makedirs(video_directory, exist_ok=True)
     # for each video, download it and put it in the directory
+    for video in videos:
+        logging.info("Start %s", video['file'])
+        r = requests.get(video['file'])
+        try:
+            r.raise_for_status()
+        except:
+            logging.warning("Unable to download video: %s", video['file'])
+            continue   
+        filename = sanitize(video['title'])
+        filepath = os.path.join(video_directory, filename)
+        with open(filepath, 'wb') as f:
+           f.write(r.content)
+        logging.info("End %s", video['file'])
 
 def main():
 
@@ -57,7 +84,8 @@ def main():
         soup = BeautifulSoup(html.unescape(response.text), "html.parser")
         playlist = get_playlist(soup)
         videos = get_best_videos(playlist)
-        print(videos)
+        lesson_title = sanitize(get_title(soup))
+        download_videos(videos, lesson_title)
 
 if __name__ == "__main__":
     main()
